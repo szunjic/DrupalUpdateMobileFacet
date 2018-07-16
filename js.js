ed1fbc9a5496a8d79dedf246102f34ec5898cf00
Sofia Zunjic <sofia.zunjic@intern-iMac.local>
2018-07-16 10:57:13 -0500

    updating random edits

--- js.js
+++ js.js
@@ -6,21 +6,33 @@ $(function() {
     // e.currentTarget
     var $this = $( e.currentTarget ); // current clicked element
     var $itemList = $( '.facetapi-facets .item-list' ); // div.item-list element
-    // if $this .hasClass facetapi-active is true
-    // prevent default
-    // .toggleClass on the $( '.facetapi-facets .item-list' ) to .show-filters
 
+    // if $this .hasClass facetapi-active is true..
     if ($this.hasClass('facetapi-active')) {
       e.preventDefault();
+    // .toggleClass on the $( '.facetapi-facets .item-list' ) to .show-filters
       $itemList.toggleClass( 'show-filters' );
     }
     // else
-
     // remove the active class (from a, and it's parent li)
-    // if $this .hasClass facetapi-active is false
-    // find element with .facetapi-active .removeClass .facetapi-active on its parent .removeClass .facetapi-item-active
-    // $this addClass .facetapi-active and to parent li addClass .facetapi-item-active
-    // .toggleClass on the .facetapi-facets .item-list to .show-filters
+    //else {
+      //$itemList.removeClass( 'facetapi-active');
+      // ** OR ..
+      // $('a').parent().removeClass('facetapi-active')
+
+
+  //  };
+
+    // if $this .hasClass facetapi-active is false..
+    if (!$this.hasClass('facetapi-active')) {
+      e.preventDefault();
+      // find element with .facetapi-active .removeClass .facetapi-active on its parent .removeClass .facetapi-item-active
+      $('.facetapi-active').parent().removeClass('.facetapi-item-active'); /// ** here using jQuery, do I need to provide a 'context' parameter ?
+      // $this addClass .facetapi-active and to parent li addClass .facetapi-item-active
+      $this.addClass('.facetapi-active').parent().addClass('.facetapi-item-active');
+      // .toggleClass on the .facetapi-facets .item-list to .show-filters
+      $itemList.toggleClass( 'show-filters' );
+    }
 
   });
 });
